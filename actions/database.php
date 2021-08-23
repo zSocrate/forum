24d8bb2439801b6b385266230b3aea1ad137ca71
zSocrate <lu.ilan@free.fr>
2021-08-23 16:36:07 +0200

    Incription terminé

--- actions/database.php
+++ actions/database.php
@@ -1,5 +1,6 @@
 <?php
 try{
+    session_start();
     $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', '');
 }catch(Exception $e){
     die('Une erreur a été trouvée : ' . $e->getMessage());
--- actions/signupAction.php
+++ actions/signupAction.php
@@ -2,22 +2,43 @@
 
 require('actions/database.php');
 
+//Validation du formulaire
 if(isset($_POST['validate'])){
 
+    //Vérifier si l'user a complété tout les champs
     if(!empty($_POST['pseudo']) && !empty($_POST['lastname']) && !empty($_POST['firstname']) && !empty($_POST['password'])){
 
+        //Les données de l'user
         $user_pseudo = htmlspecialchars($_POST['pseudo']);
         $user_lastname = htmlspecialchars($_POST['lastname']);
         $user_firstname = htmlspecialchars($_POST['firstname']);
         $user_password = password_hash($_POST['password'] , PASSWORD_DEFAULT);
 
+        //Vérifier si l'utilisateur existe déjà sur le site
         $checkIfUserAlreadyExists = $bdd->prepare('SELECT pseudo FROM users WHERE pseudo = ?');
-        $checkIfUserAlreadyExists->execute(array($user_pseudo));
+        $checkIfUserAlreadyExists->execute([$user_pseudo]);
 
         if($checkIfUserAlreadyExists->rowCount() == 0){
             
+            //Insérer l'utilisateur dans la bdd
             $insertUserOnWebsite = $bdd->prepare('INSERT INTO users(pseudo, lastname, firstname, pass)VALUE(?, ?, ?, ?)');
-            $insertUserOnWebsite->execute(array($user_pseudo, $user_lastname, $user_firstname, $user_password));
+            $insertUserOnWebsite->execute([$user_pseudo, $user_lastname, $user_firstname, $user_password]);
+
+            //Récupérer les informations de l'utilisateur
+            $getInfosOfThisUserReq = $bdd->prepare('SELECT id FROM users WHERE nom = ? AND prenom = ? AND pseudo = ?');
+            $getInfosOfThisUserReq->execute([$user_lastname, $user_firstname, $user_pseudo]);
+
+            $usersInfos = $getInfosOfThisUserReq->fetch();
+
+            //Authentifier l'utilisateur sur le site et récupérer ses données dans des variables globales sessions
+            $_SESSION['auth'] = true;
+            $_SESSION['id'] = $usersInfos['id'];
+            $_SESSION['lastname'] = $usersInfos['lastname'];
+            $_SESSION['firstname'] = $usersInfos['firstname'];
+            $_SESSION['pseudo'] = $usersInfos['pseudo'];
+
+            //Rediriger l'utilisateur vers la page d'acceuil
+            header('Location: index.php');
 
         }else{
             $errorMsg = "L'utilisateur existe déjà sur le site";
